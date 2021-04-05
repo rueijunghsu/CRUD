1c852afa1b36dd894d5de86c91a68054ec6e6f8f
Ray Hsu <ray199185@gmail.com>
2021-04-05 13:04:16 +0800

    new and create done

---
+++ vote_candidate02/db/migrate/20210405034523_create_candidates.rb
@@ -0,0 +1,13 @@
+class CreateCandidates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :candidates do |t|
+      t.string :name
+      t.integer :age
+      t.string :party
+      t.text :politics
+      t.integer :votes, default: 0
+
+      t.timestamps
+    end
+  end
+end
