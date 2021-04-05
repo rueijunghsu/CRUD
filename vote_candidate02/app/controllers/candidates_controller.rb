c451c083d8509679270180cfe07ff20d2f0d8176
Ray Hsu <ray199185@gmail.com>
2021-04-05 14:22:05 +0800

    edit and destroy done

--- vote_candidate02/app/controllers/candidates_controller.rb
+++ vote_candidate02/app/controllers/candidates_controller.rb
@@ -11,10 +11,30 @@ class CandidatesController < ApplicationController
     @candidate = Candidate.new(candidate_params)
 
     if @candidate.save
-      redirect_to "candidates_path", notice: "新增候選人成功!"
+      redirect_to candidates_path, notice: "新增候選人成功!"
     else
       render :new
     end
+  end
+
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+    
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "候選人資料更新成功!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy if @candidate
+    redirect_to candidates_path, notice: "候選人資料刪除成功!"
   end
 
   private
